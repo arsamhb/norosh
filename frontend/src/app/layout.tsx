dfdbc207875041f0f6f34b77ec21b95bed427322
Arsam <arsam.hb@gmail.com>
2024-09-08 18:50:54 +0330

    feat: generic tab manager

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,21 +1,39 @@
 'use client'
-import Footer from "@/components/layout/Footer";
 import "./globals.css";
-import Nav from "@/components/layout/Nav";
+import Footer from "@/components/layout/main/Footer";
+import Nav from "@/components/layout/main/Nav";
+import TabController from "@/components/layout/panel/TabController";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const tabs = [
+    {
+      navigateTo: "/test1",
+      title: "تایتل اول",
+    },
+    {
+      navigateTo: "/test2",
+      title: "تایتل دوم",
+    },
+    {
+      navigateTo: "/test3",
+      title: "تایتل سوم",
+    },
+  ]
+
   return (
     <html lang="en" dir="rtl">
       <body className="min-h-screen max-w-screen w-screen flex flex-col">
-        <Nav />
+        {/* <Nav /> */}
+        
         <main className="grow grid place-items-center">
           {children}
         </main>
-        <Footer />
+        <TabController tabsList={tabs} />
+        {/* <Footer /> */}
       </body>
     </html>
   );
